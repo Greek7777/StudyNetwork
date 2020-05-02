dd2fac1e45f0d7c2ebad63d739e5405ae19450c9
Greek7777 <63196668+Greek7777@users.noreply.github.com>
2020-05-02 16:16:02 +0300

    Update program2.cpp

--- work/program2.cpp
+++ work/program2.cpp
@@ -34,7 +34,7 @@ int main(int argc, char **argv)
     int msgLen = strlen(buf);        // вычисляем длину строки
 
     // создать сокет
-    int mySocket = socket(AF_INET, SOCK_DGRAM, 0); // TCP
+    int mySocket = socket(AF_INET, SOCK_DGRAM, 0); // UDP
     if (mySocket == -1) {
         errHandler("Error open socket", 11);
     }
